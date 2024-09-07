dbbd2928eba086c74360622a0ba5f62296d40f0b
RicardoMimoHalak <ricardo.halak@gmail.com>
2024-09-07 09:22:14 +0200

    Change

--- counter.py
+++ counter.py
@@ -1,3 +1,5 @@
+# A counter is a container that stores elements as dictionary keys, and their counts are stored as dictionary values.
+
 from collections import Counter
 
 myList = [1,1,2,3,4,5,3,2,3,4,2,1,2,3]
