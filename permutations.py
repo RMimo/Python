66ad6c459cb67370ae7a2833cf71f6eef274c09b
Ricardo Mimo Halak <106038660+RMHalak@users.noreply.github.com>
2024-09-08 10:35:42 +0200

    Update permutations.py

--- permutations.py
+++ permutations.py
@@ -1,5 +1,6 @@
 # This tool returns successive r-length permutations of elements in an iterable.
-# If r is not specified or is None, then  defaults to the length of the iterable, and all possible full length permutations are generated. Permutations are printed in a lexicographic sorted order. So, if the input iterable is sorted, the permutation tuples will be produced in a sorted order.
+# If r is not specified or is None, then  defaults to the length of the iterable, and all possible full length permutations are generated. 
+# Permutations are printed in a lexicographic sorted order. So, if the input iterable is sorted, the permutation tuples will be produced in a sorted order.
 
 from itertools import permutations
 print(permutations(['1','2','3']))
