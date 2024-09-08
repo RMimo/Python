29b8233f4ec5558aef659c6f69f7fbd624d7336f
Ricardo Mimo Halak <106038660+RMHalak@users.noreply.github.com>
2024-09-08 10:35:56 +0200

    Update combinations.py

--- combinations.py
+++ combinations.py
@@ -1,5 +1,4 @@
 # This tool returns the  length subsequences of elements from the input iterable.
-
 # Combinations are emitted in lexicographic sorted order. So, if the input iterable is sorted, the combination tuples will be produced in sorted order.
 
 from itertools import combinations
