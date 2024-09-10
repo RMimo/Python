b1ec800bbacd40320a23f0b3c93ff045c938ee0b
Ricardo Mimo Halak <106038660+RMHalak@users.noreply.github.com>
2024-09-10 09:58:57 +0200

    Create divmod.py

---
+++ math/divmod.py
@@ -0,0 +1,10 @@
+# One of the built-in functions of Python is divmod, which takes two arguments a and b and returns a tuple containing the quotient of a/b first and then the remainder a.
+
+a = 177
+b = 10
+
+print(a//b)
+print(a%b)
+print(divmod(a,b))
+
+# Here, the integer division is 177/10 => 17 and the modulo operator is 177%10 => 7.
