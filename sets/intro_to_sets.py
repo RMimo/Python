e33e7cb41d310b75fe6cf4347b701d75deaa5f1c
Ricardo Mimo Halak <106038660+RMHalak@users.noreply.github.com>
2024-09-11 09:39:01 +0200

    Create intro_to_sets.py

---
+++ sets/intro_to_sets.py
@@ -0,0 +1,11 @@
+# A set is an unordered collection of elements without duplicate entries.
+# When printed, iterated or converted into a sequence, its elements will appear in an arbitrary order.
+# Basically, sets are used for membership testing and eliminating duplicate entries.
+
+print(set())
+print(set('Introduction to Sets'))
+print(set([1,2,1,2,3,4,5,6,0,9,12,22,3]))
+print(set((1,2,3,4,5,5)))
+print(set(set(['I', 'n', 't', 'r', 'o', 'd', 'u', 'c', 't', 'i', 'o', 'n', 't', 'o', 'S', 'e', 't', 's'])))
+print(set({'Introduction' : 'DOSHI', 'Sets' : 616 }))
+print(set(enumerate(['I', 'n', 't', 'r', 'o', 'd', 'u', 'c', 't', 'i', 'o', 'n', 't', 'o', 'S', 'e', 't', 's'])))
