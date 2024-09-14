8c833a3bbf40c7d17ee52ea689ad3158a94c159b
RicardoMimoHalak <ricardo.halak@gmail.com>
2024-09-14 11:52:31 +0200

    .

--- regex_parsing/finditer.py
+++
@@ -1,4 +0,0 @@
-# The expression re.finditer() returns an iterator yielding MatchObject instances over all non-overlapping matches for the re pattern in the string.
-import re
-print(re.finditer(r'\w','http://www.introregex.com/'))
-print(*map(lambda x: x.group(), re.finditer(r'\w','http://www.introregex.com/')))
