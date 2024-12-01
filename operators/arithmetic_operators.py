b536af4752ea9f4d630b7450d16e24fe7b343f2d
Ricardo Mimo Halak <ricardo.halak@gmail.com>
2024-12-01 14:43:06 +0100

    add arithmetic operators

---
+++ operators/arithmetic_operators.py
@@ -0,0 +1,28 @@
+# source: https://www.w3schools.com/python/python_operators.asp
+
+a = 10
+b = 3
+
+print('A:', a)
+print('B:', b)
+
+# addition
+print('Addition:', a + b)
+
+# subtraction
+print('Subtraction:', a - b)
+
+# multiplication
+print('Multiplication:', a * b)
+
+# division
+print('Division:', a / b)
+
+# modulus
+print('Modulus:', a%b)
+
+# exponentiation
+print('Exponentiation:', a**b)
+
+# floor division
+print('Floor Division:', a//b)
