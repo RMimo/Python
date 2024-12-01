1ec60bf5a0536cff6eec46c8e616763834d21e4b
Ricardo Mimo Halak <ricardo.halak@gmail.com>
2024-12-01 16:05:30 +0100

    add membership operators

---
+++ operators/membership_operators.py
@@ -0,0 +1,20 @@
+# source: https://www.w3schools.com/python/python_operators.asp3
+
+# Membership operators are used to test if a sequence is presented in an object
+
+a = [[1, 2], 3, 'my_string']
+
+b = 1
+c = "my"
+d = "my_string"
+e = [1, 2]
+
+print("b in a", b in a)
+print("c in a", c in a)
+print("d in a", d in a)
+print("e in a", e in a)
+
+print("b not in a", b not in a)
+print("c not in a", c not in a)
+print("d not in a", d not in a)
+print("e not in a", e not in a)
