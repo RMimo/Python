6d22ff8c507e6b5f937980128feef66af551e83e
Ricardo Mimo Halak <ricardo.halak@gmail.com>
2024-12-01 15:49:27 +0100

    add comparison operators

---
+++ operators/comparison_operators.py
@@ -0,0 +1,24 @@
+# source: https://www.w3schools.com/python/python_operators.asp3
+
+# Comparison operators are used to compare two values
+
+def comparison_operators(operation: str, a: int = 10, b: int = 3):
+    if operation == "==": 
+        return print("a == b", a == b)
+    elif operation == "!=": 
+        return print("a != b", a != b)
+    elif operation == ">": 
+        return print("a > b", a > b)
+    elif operation == "<": 
+        return print("a < b", a < b)
+    elif operation == ">=": 
+        return print("a >= b", a >= b)
+    elif operation == "<=": 
+        return print("a <= b", a <= b)
+    
+comparison_operators("==")
+comparison_operators("!=")
+comparison_operators(">")
+comparison_operators("<")
+comparison_operators(">=")
+comparison_operators("<=")
